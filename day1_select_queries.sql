e10e5d4c5c53d01d1a04646add99b2fd41af0324
Zaid-Abotahoon <zaidabotahoondan18@gmail.com>
2026-08-30 01:41:10 +0300

    Update day1_select_queries.sql

--- day1_select_queries.sql
+++ day1_select_queries.sql
@@ -1,3 +1,21 @@
 -- Retrive All Customer Data 
 SELECT * 
-FROM customers ;
+FROM customers 
+
+-- Retrive All Order Data 
+SELECT * 
+FROM orders 
+
+-- Retrive each customer's name , country , and score.
+SELECT  first_name , Country , Score 
+fROM customers;
+
+-- Retrieve customers with a score not equal to O
+SELECT * 
+FROM customers 
+WHERE score  != 0
+
+-- Retrieve customers from Germany
+SELECT * 
+FROM customers 
+WHERE country = 'Germany'
