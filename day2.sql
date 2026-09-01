271d51ea900484d558f5bd0d20d7cd530b435857
Zaid-Abotahoon <zaidabotahoondan18@gmail.com>
2026-09-01 15:59:50 +0300

    Update day2.sql

--- day2.sql
+++ day2.sql
@@ -1,1 +1,37 @@
+/* Retrieve all customers and 
+sort the results by the highest score first */
+SELECT * 
+FROM customers 
+ORDER BY score DESC
 
+
+/* Retrieve all customers and
+sort the results by the lowest score first */
+SELECT * 
+FROM customers 
+ORDER BY score ASC
+
+
+
+/* Retrieve all customers and
+sort the results by the country
+and then by the highest score */
+SELECT * 
+FROM customers 
+ORDER BY country ASC , score DESC
+
+
+-- Find the total score for each country
+SELECT 
+	country , 
+	SUM (score) AS total_score
+FROM customers
+GROUP BY country
+
+-- Find the total score and total number of customers for each country 
+SELECT 
+	country ,
+	SUM (score) AS total_score ,
+	COUNT (id) AS total_customers
+	FROM customers 
+	GROUP BY country  
