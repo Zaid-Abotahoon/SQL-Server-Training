79230c60fe4d57d56130142bbdce7d0c1e31abcb
Zaid-Abotahoon <zaidabotahoondan18@gmail.com>
2026-09-03 02:54:09 +0300

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,1 +1,9 @@
+INSERT INTO  customers (id, first_name, country, score) 
+VALUES 
+	(6 , 'Ahmad' , 'Jordan' , NULL) ,  
+	(7 , 'Anas' , 'Saudi Arabia' , 432); 
+
+INSERT INTO  customers 
+VALUES
+	(8 , 'Rami' , Null , Null); 
 
