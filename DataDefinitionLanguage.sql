677289d21133fc2cbf79636bab775e3c65aa7602
Zaid-Abotahoon <zaidabotahoondan18@gmail.com>
2026-09-03 01:23:00 +0300

    Update DataDefinitionLanguage.sql

--- DataDefinitionLanguage.sql
+++ DataDefinitionLanguage.sql
@@ -1,1 +1,8 @@
+/* Create a new table called persons
+with columns: id, person name, birth date, and phone */
+CREATE TABLE persons (
+	id INT NOT NULL ,
+	person_name VARCHAR(40) NOT NULL , 
+	birth_date DATE 
 
+)
