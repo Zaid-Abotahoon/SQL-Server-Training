e5dbf4089b908c349f689e9d89d5444fcc431375
Zaid-Abotahoon <zaidabotahoondan18@gmail.com>
2026-09-03 01:46:32 +0300

    Update DataDefinitionLanguage.sql

--- DataDefinitionLanguage.sql
+++ DataDefinitionLanguage.sql
@@ -3,6 +3,23 @@ with columns: id, person name, birth date, and phone */
 CREATE TABLE persons (
 	id INT NOT NULL ,
 	person_name VARCHAR(40) NOT NULL , 
-	birth_date DATE 
+	birth_date DATE ,
+	phone VARCHAR(10) NOT NULL
+	CONSTRAINT pk_persons PRIMARY KEY (id)
 
-)
+);
+
+-- Add a new column called email to the persons table
+ALTER TABLE persons
+	ADD email VARCHAR(20) NOT NULL;
+	 
+
+-- Remove the column phone from the persons table
+ALTER TABLE persons 
+	DROP COLUMN phone;
+
+
+-- Delete the table persons from the database
+DROP TABLE persons ;
+
+
